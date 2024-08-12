696a1742786801b436e07d1a306e9fd18486bf91
Nabil0914 <nabilafraariz@gmail.com>
2024-08-12 22:27:29 +0530

    made hash password

--- backend/src/models/admin.model.js
+++ backend/src/models/admin.model.js
@@ -22,4 +22,19 @@ const adminSchema = new mongoose.Schema({
     }
 },{timestamps : true})
 
+adminSchema.pre("save", async function(next) {
+
+    // Check if the password field is modified
+    if(!this.isModified("password")) return next();
+    
+    
+    // Hash the password with a salt factor of 10
+    this.password = await bcrypt.hash(this.password, 10)
+    next();
+    })
+    
+    adminSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+    }
+
 export const Admin = mongoose.model("Admin",adminSchema);
--- backend/src/models/teacher.models.js
+++ backend/src/models/teacher.models.js
@@ -30,4 +30,19 @@ const teacherSchema = new mongoose.Schema(
     {timestamps: true}
 )
 
+teacherSchema.pre("save", async function(next) {
+
+    // Check if the password field is modified
+    if(!this.isModified("password")) return next();
+    
+    
+    // Hash the password with a salt factor of 10
+    this.password = await bcrypt.hash(this.password, 10)
+    next();
+    })
+    
+    teacherSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+    }
+
 export const Teacher = mongoose.model("Teacher", teacherSchema);
