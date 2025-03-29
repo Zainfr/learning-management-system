faea16a78dc50b3e2b7ff172ecb863b643d622dd
Aquib <aquibpc@gmail.com>
2025-03-29 20:41:28 +0530

    get api to get students assc to batch

--- backend/src/controllers/createBatchController.js
+++ backend/src/controllers/createBatchController.js
@@ -1,4 +1,5 @@
 import { Batch } from "../models/batch.model.js";
+import { Student } from "../models/student.model.js";
 
 export const createBatch = async (req, res) => {
     try {
@@ -20,3 +21,25 @@ export const createBatch = async (req, res) => {
         }
     }
 }
+
+export const getStudentsInBatch = async (req, res) => {
+    try {
+        const { batchNo } = req.params;
+
+        // Find the batch document by batchNo
+        const batch = await Batch.findOne({ batchNo: batchNo });
+
+        if (!batch) {
+            return res.status(404).json({ message: "Batch not found" });
+        }
+
+        // Find students associated with the batch
+        const students = await Student.find({ batch: batch._id })
+        .populate("sem", "name")  // Populating Semester Name
+        .select("name rollno mobile sem");
+        res.status(200).json(students);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
--- backend/src/routes/lmsRoute.js
+++ backend/src/routes/lmsRoute.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { createBatch } from '../controllers/createBatchController.js';
+import { getStudentsInBatch } from '../controllers/createBatchController.js';
 
 const router = express.Router();
 
-router.post('/create-batch',createBatch);
+router.post('/create-batch', createBatch);
+router.get('/get-students/:batchNo', getStudentsInBatch);
 
 export default router;
