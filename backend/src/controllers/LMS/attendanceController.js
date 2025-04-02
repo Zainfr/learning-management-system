cac4ae6f6896d235eb39a42c0a51206db7b11954
Aquib <aquibpc@gmail.com>
2025-04-02 22:59:23 +0530

    mark attendance api

--- backend/src/controllers/LMS/attendanceController.js
+++ backend/src/controllers/LMS/attendanceController.js
@@ -1,6 +1,7 @@
 import { Attendance } from "../../models/LMS/attendance.model.js";
 import { Batch } from "../../models/LMS/batch.model.js";
 import { Lecture } from "../../models/LMS/lecture.model.js";
+import { Student } from "../../models/student.model.js";
 
 
 export const createLecture = async (req, res) => {
@@ -8,8 +9,8 @@ export const createLecture = async (req, res) => {
 
     try {
         const doesBatchExists = await Batch.findById(batch);
-        if(!doesBatchExists){
-            return res.status(400).json({success : false, message : "Batch not found"});
+        if (!doesBatchExists) {
+            return res.status(400).json({ success: false, message: "Batch not found" });
         }
 
         const lecture = await Lecture.create({
@@ -19,13 +20,94 @@ export const createLecture = async (req, res) => {
             batch,
             teacher
         })
-        if(!lecture){
-            return res.status(400).json({success : false,message : "There was a Error creating the lecture"});
+        if (!lecture) {
+            return res.status(400).json({ success: false, message: "There was a Error creating the lecture" });
         }
 
-        return res.status(201).json({success : true,message : "Lecture created successfully",lecture});
+        return res.status(201).json({ success: true, message: "Lecture created successfully", lecture });
     } catch (error) {
         console.log(error);
-        return res.status(500).json({success : false,message : "Internal Server Error"});
+        return res.status(500).json({ success: false, message: "Internal Server Error" });
+    }
+}
+
+export const markAttendance = async (req, res) => {
+    const { batchId, lectureId, absentRollnos, markedBy } = req.body;
+
+    console.log("Batch ID:", batchId);
+    console.log("Lecture ID:", lectureId);
+    console.log("Absent Roll Numbers:", absentRollnos);
+
+
+    try {
+        const doesLectureExists = await Lecture.findById(lectureId);
+        if (!doesLectureExists) {
+            return res.status(400).json({ success: false, message: "Lecture/Lab not found" });
+        }
+
+        const batchStudents = await Student.find({ batch: batchId });
+        if (batchStudents.length === 0) {
+            return res.status(404).json({ success: false, message: "No students found in this batch" });
+        }
+        console.log(batchStudents.length);
+        const absentRollnosToUpperCase = absentRollnos.map((rollno) => rollno.toUpperCase());
+
+        const attendanceRecord = [];
+
+        let presentCount = 0;
+        let absentCount = 0;
+        const errors = [];
+
+        for (const student of batchStudents) {
+            const isAbsent = absentRollnosToUpperCase.includes(student.rollno);
+
+            const attendanceObj = {
+                lecture: lectureId,
+                student: student._id,
+                status: isAbsent ? 'Absent' : 'Present',
+                markedby: markedBy,
+            }
+
+            attendanceRecord.push(attendanceObj);
+
+            if (isAbsent) {
+                absentCount++;
+            } else {
+                presentCount++;
+            }
+        }
+
+        try {
+            await Attendance.insertMany(attendanceRecord, { ordered: false });
+        } catch (err) {
+            if (err.code === 11000) {
+                errors.push({ message: "Attendance already marked for this student" });
+            } else {
+                console.error("Error inserting attendance:", err);
+                errors.push({ message: "Error marking attendance" });
+            }
+        }
+        // if (batchStudents.length > 0) {
+        //     try {
+        //         const singleRecord = new Attendance({
+        //             lecture: lectureId,
+        //             student: batchStudents[0]._id,
+        //             status: 'Present',
+        //             markedBy: markedBy
+        //         });
+        //         await singleRecord.save();
+        //         console.log("Manual single record saved successfully");
+        //     } catch (singleInsertError) {
+        //         console.error("Manual single insert failed:", singleInsertError);
+        //     }
+        // }
+
+
+        return res.status(201).json({ success: true, message: "Attendance marked successfully" });
+
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ success: false, message: "Internal Server Error" });
     }
 }
--- backend/src/routes/lmsRoute.js
+++ backend/src/routes/lmsRoute.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { createBatch } from '../controllers/createBatchController.js';
 import { getStudentsInBatch } from '../controllers/createBatchController.js';
-import { createLecture } from '../controllers/LMS/attendanceController.js';
+import { createLecture, markAttendance } from '../controllers/LMS/attendanceController.js';
 
 const router = express.Router();
 
 router.post('/create-batch', createBatch);
 router.get('/get-students/:batchNo', getStudentsInBatch);
 router.post('/create-lecture',createLecture);
+router.post('/mark-attendance',markAttendance);
 
 export default router;
